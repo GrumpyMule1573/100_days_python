61360dd3dc699fc2a93cc45a8f93e3f430bbecbf
GrumpyMule1573 <114995652+GrumpyMule1573@users.noreply.github.com>
2022-10-04 15:36:00 +0100

    Update Caesar_cipher_encode_my_version.py

--- Day_8/Caesar_cipher_encode_my_version.py
+++ Day_8/Caesar_cipher_encode_my_version.py
@@ -5,25 +5,23 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-	to_encrypt = text
+def encrypt(plain_text, shift_amount):
 	to_encrypt_index = []
-	shift = int(shift)
 	encrypted = ""
 
 # grab alphabet indices of the word
 # this matches the given word letter with the letter in alphabet and appends index number
-	for letter in to_encrypt:
+	for letter in plain_text:
 		to_encrypt_index.append(alphabet.index(letter))
 
 # create the shifted index - if > 25, -26 to get back to index[0]
 # appends the letter found at the new index to encrypted
 	for new_index in to_encrypt_index:
-		plus_shift = int(new_index) + shift
+		plus_shift = new_index + shift_amount
 		if plus_shift > 25:
 			plus_shift -= 26
 		encrypted += alphabet[plus_shift]
 	
-	print(f"The encrypted message is {encrypted})
+	print(f"The encrypted message is {encrypted}")
     
-encrypt("hello",5)
+encrypt(plain_text=text, shift_amount=shift)
