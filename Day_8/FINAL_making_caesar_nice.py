83ef628b30986403101a21dca4ec8e55fcaceac9
GrumpyMule1573 <114995652+GrumpyMule1573@users.noreply.github.com>
2022-10-04 18:02:51 +0100

    Update and rename making_caesar_nice.py to FINAL_making_caesar_nice.py

--- Day_8/FINAL_making_caesar_nice.py
+++ Day_8/FINAL_making_caesar_nice.py
@@ -5,9 +5,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 def caesar(user_text, shift_amount, user_choice):
 
 	output_text = ""
+# accounts for shifts >26 by using the remainder as the shift amount
+	shift_amount = shift_amount % 26
 
+# changes shift to a negative for decoding
 	if user_choice == "decode":
 		shift_amount *= -1
+		
 	for letter in user_text:
 # to account for special characters or spaces
 		if letter not in alphabet:
@@ -16,12 +20,6 @@ def caesar(user_text, shift_amount, user_choice):
 		else:
 			position = alphabet.index(letter)
 			new_position = position + shift_amount
-# accounts for shift entry of > 25 or < -25 to keep within alphabet range
-			while new_position > 25 or new_position < -25:
-				if new_position > 25:
-					new_position -= 26
-				elif new_position < -25:
-					new_position += 26
 			output_text += alphabet[new_position]
 	
 	print(f"The {user_choice}d message is {output_text}")
