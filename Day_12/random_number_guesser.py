8e6e4f295b14cea275b47edb581b1f1c5d982cf3
GrumpyMule1573 <114995652+GrumpyMule1573@users.noreply.github.com>
2022-10-12 11:31:35 +0100

    Update random_number_guesser.py

--- Day_12/random_number_guesser.py
+++ Day_12/random_number_guesser.py
@@ -19,8 +19,13 @@ def difficulty_choice():
 #user enters a guess
 def user_guess():
 	"""Player chooses a number to guess"""
-	the_guess = int(input("Choose a number between 0 and 100\n"))
-	return the_guess
+	valid_guess = False
+	while not valid_guess:
+		the_guess = input("Choose a number between 0 and 100\n")
+		if type(the_guess) is int and 0 < the_guess < 100:
+			return the_guess
+		else:
+			print("Please enter a valid number")
 
 #hot or cold for how close they are to the number
 def hot_or_cold(the_guess, computer_number):
@@ -46,43 +51,58 @@ def art_guesses_left(user_guesses):
 
 def guess_the_number():
 	"""Player guesses the number"""
-	print(logo)
-	computer_number = number_to_guess()
-	difficulty = difficulty_choice()
-	user_guesses = 0
-## set number of guesses
-	if difficulty == "hard":
-		user_guesses = 5
-		print(hard_choice)
-		art_guesses_left(user_guesses)
-	else:
-		user_guesses = 10
-		print(easy_choice)
-		art_guesses_left(user_guesses)
-
-	the_guess = user_guess()
-	
-## assess how close user is to number
-	while user_guesses > 1:
-		if the_guess < computer_number:
-			user_guesses -= 1			
-			print("Too low! Guess again.")
-			hot_or_cold(the_guess, computer_number)
+	keep_playing = True
+	while keep_playing:
+		print(logo)
+		computer_number = number_to_guess()
+		difficulty = difficulty_choice()
+		user_guesses = 0	
+	## set number of guesses
+		if difficulty == "hard":
+			user_guesses = 5
+			print(hard_choice)
 			art_guesses_left(user_guesses)
-			the_guess = user_guess()
-		elif the_guess > computer_number:
-			user_guesses -= 1
-			print("Too high! Guess again.")
-			hot_or_cold(the_guess, computer_number)
-			art_guesses_left(user_guesses)
-			the_guess = user_guess()
 		else:
-			user_guesses = 0
+			user_guesses = 10
+			print(easy_choice)
+			art_guesses_left(user_guesses)
+	
+		the_guess = user_guess()
+		
+	## assess how close user is to number
+		while user_guesses > 1:
+			if the_guess < computer_number:
+				user_guesses -= 1			
+				print("Too low! Guess again.")
+				hot_or_cold(the_guess, computer_number)
+				art_guesses_left(user_guesses)
+				the_guess = user_guess()
+			elif the_guess > computer_number:
+				user_guesses -= 1
+				print("Too high! Guess again.")
+				hot_or_cold(the_guess, computer_number)
+				art_guesses_left(user_guesses)
+				the_guess = user_guess()
+			else:
+				user_guesses = 0
+				print(winner)
+				print(f"You guessed the number {the_guess} correctly! You win!")
+		if computer_number != the_guess:
+			print(loser)
+			print(f"You ran out of guesses! The computer number was {computer_number}")
+		else:
 			print(winner)
 			print(f"You guessed the number {the_guess} correctly! You win!")
-	if computer_number != the_guess:
-		print(loser)
-		print(f"You ran out of guesses! The computer number was {computer_number}")
+		are_you_playing = ""
+		while are_you_playing != "y" and are_you_playing != "n":
+			are_you_playing = input("Would you like to play again? y/n ")
+			if are_you_playing == "y":
+				os.system("clear")
+			elif are_you_playing == "n":
+				print("Thank you for playing!")
+				keep_playing = False
+			else:
+				print("Please enter a valid response.")
 
 
 
